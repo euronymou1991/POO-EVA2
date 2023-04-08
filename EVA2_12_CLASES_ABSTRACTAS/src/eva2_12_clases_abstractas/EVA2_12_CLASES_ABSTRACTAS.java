83cea1584b36e728f6581100378d15a75497cccb
euronymou1991 <124392477+euronymou1991@users.noreply.github.com>
2023-04-08 10:41:40 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,20 @@
+
+package eva2_12_clases_abstractas;
+
+
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    public static void main(String[] args) {
+    
+        Prueba prueba=new Prueba();
+        PruebaAbstracta pruebaAbs=new PruebaAbstracta();
+    }
+    
+}
+class Prueba{
+    
+}
+//NO SE PUEDE INSTANCIAR
+abstract class PruebaAbstracta{
+    
+}
