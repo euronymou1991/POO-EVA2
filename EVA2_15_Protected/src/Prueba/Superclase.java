6e56e8838e9281bd6727f82d2114164673eba7ff
euronymou1991 <124392477+euronymou1991@users.noreply.github.com>
2023-04-08 10:50:46 -0600

    Add files via upload

---
+++ EVA2_15_Protected/src/Prueba/Superclase.java
@@ -0,0 +1,15 @@
+
+package Prueba;
+
+public class Superclase {
+    protected int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+}
---
+++ EVA2_15_Protected/src/eva2_15_protected/EVA2_15_Protected.java
@@ -0,0 +1,21 @@
+
+package eva2_15_protected;
+
+import Prueba.Superclase;
+
+
+public class EVA2_15_Protected {
+
+   
+    public static void main(String[] args) {
+       
+     
+    }
+    
+}
+
+class SubClase extends Superclase{
+      public void imprimirValor(){
+          System.out.println("El valor es: "+valor);
+      }
+}
